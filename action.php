35d6f1d4d4b29837d2240dc33a5941626fa05f1c
Michael Annor <m.annor13@gmail.com>
2015-10-27 21:26:52 +0000

    fixed add to cart popup

--- action.php
+++ action.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /**
  * author:
@@ -9,14 +9,34 @@
 $cmd = $_REQUEST['cmd'];
 switch ($cmd) {
   case 1:
+  include ("product.php");
+    $pid = $_REQUEST['productid'];
+    $obj = new product();
+
+$row = $obj->get_product_by_id($pid);
+    //return a JSON string to browser when request comes to get description
+
+    if($row){
+    //generate the JSON message to echo to the browser
+      echo '{"result":1,"product":[';	//start of json object
+      echo json_encode($row);			//convert the result array to json object
+      echo "]}";							//end of json array and object
+    }
+    else{
+      echo '{"result":0,"message": "product not got."}';
+    }
+
+
+
     # code...
     break;
 
   case 2:
     # code...
     break;
-    
+
   default:
     # code...
     break;
 }
+?>
