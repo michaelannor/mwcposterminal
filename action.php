3f08d1ab23a155956d33220a457d20410d1e1035
Michael Annor <m.annor13@gmail.com>
2015-10-27 14:55:21 +0000

    tracking the php action page

---
+++ action.php
@@ -0,0 +1,22 @@
+<?
+
+/**
+ * author:
+ * date:
+ * description:
+ */
+
+$cmd = $_REQUEST['cmd'];
+switch ($cmd) {
+  case 1:
+    # code...
+    break;
+
+  case 2:
+    # code...
+    break;
+    
+  default:
+    # code...
+    break;
+}
