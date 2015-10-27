cc876bc80c6db96500e0b9b2003cf7168e0f04a1
Michael Annor <m.annor13@gmail.com>
2015-10-27 08:52:18 +0000

    added functions in the transaction class. also fixed another set of running through typos in the queries

--- product.php
+++ product.php
@@ -3,7 +3,7 @@ include("adb.php");
 
 class product extends adb{
   /**
-   * [[The add_product is a fuction to add a product to the database]]
+   * [[The add_product is a function to add a product to the database]]
    * @param [[Int]] $id [[Product id, represented by a barcode]]
    * @param [[Varchar]] $name  [[Name of Product]]
    * @param [[Decimal]] $price  [[Price of Product]]
@@ -16,7 +16,7 @@ class product extends adb{
     }
 
   /**
-   * [[The get_all_products is a fuction to fetch all the products from the database]]
+   * [[The get_all_products is a function to fetch all the products from the database]]
    */
     function get_all_products(){
         $str_query="select * from pos_product";
@@ -27,7 +27,7 @@ class product extends adb{
     }
 
   /**
-   * [[The get_product_by_id is a fuction to fetch a specific product from the database by id]]
+   * [[The get_product_by_id is a function to fetch a specific product from the database by id]]
    * @param [[Int]] $id [[Product id, represented by a barcode]]
    */
     function get_product_by_id($id){
---
+++ transaction.php
@@ -0,0 +1,69 @@
+<?php
+include("adb.php");
+
+class transaction extends adb{
+  /**
+   * [[The add_transaction is a function to record a transaction in the database]]
+   * @param [[Date]] $date [[The date of the transaction]]
+   * @param [[Time]] $time  [[The time of the transaction]]
+   * @param [[Varchar]] $customer  [[The customer's phone number]]
+   */
+    function add_transaction($date, $time, $customer){
+        $str_query="insert into pos_transaction set date='$date',
+        time='$time',customer='$customer'";
+        return $this->query($str_query);
+    }
+
+  /**
+   * [[The get_all_transactions is a function to fetch all the transactions from the database]]
+   */
+    function get_all_transactions(){
+        $str_query="select * from pos_transaction";
+        if(!$this->query($str_query)){
+            return false;
+        }
+        return $this->fetch();
+    }
+
+  /**
+   * [[The get_transaction_by_id is a function to fetch a specific transaction from the database by id]]
+   * @param [[Int]] $id [[Product id, represented by a barcode]]
+   */
+    function get_transaction_by_id($id){
+        $str_query="select * from pos_transaction where transaction_id='$id'";
+        if(!$this->query($str_query)){
+            return false;
+        }
+        return $this->fetch();
+    }
+
+
+      /**
+       * [[The get_transactions_above_value is a function to fetch transactions above a threshold from the database by id]]
+       * @param [[Decimal]] $threshold [[a threhold value to distinguish high value transactions]]
+       */
+        function get_transactions_above_value($threshold){
+            $str_query="select * from pos_transaction where value>='$threshold'";
+            if(!$this->query($str_query)){
+                return false;
+            }
+            return $this->fetch();
+        }
+
+      /**
+       * [[The set_transaction_value is a function to set the value (total cost) of a transaction by id]]
+       * @param [[Int]] $id [[Transaction id]]
+       * @param [[Decimal]] $value [[The totlal value of the transaction]]
+       */
+        function set_transaction_value($id, $value){
+            $str_query="update pos_transaction set value = $value where transaction_id='$id'";
+            if(!$this->query($str_query)){
+                return false;
+            }
+            return $this->fetch();
+        }
+
+
+
+}
+?>
